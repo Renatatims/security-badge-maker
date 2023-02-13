27160bff6a03a50a3ac21bb01ff3c7b4a73e3a5c
ReTims <renatatims@gmail.com>
2023-02-13 17:35:56 -0600

    Updated main-method - included an interactive command-line program - User input employee's names

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: ");
+                // Get a name from the console and assign it to a variable
+                //?? - similar to a ternary operator - if input is null then an empty string will be placed"
+                string input = Console.ReadLine() ?? "";
+                // Break if the user hits ENTER without typing a name
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+    }
+}
