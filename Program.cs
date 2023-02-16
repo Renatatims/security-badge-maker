3f9f6ed005a9d7085b1aca1d2f3a03caf7e05de8
ReTims <renatatims@gmail.com>
2023-02-16 11:53:40 -0600

    Added an inital prompt so the user can choose to manually input the data or grab the data from the API in order to generate the badges - if statement - Program.cs

--- Program.cs
+++ Program.cs
@@ -6,25 +6,41 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-      
+
         // Main - asynchronous
         async static Task Main(string[] args)
         {
-            // GetEmployees method call
-            //List<Employee> employees = PeopleFetcher.GetEmployees();
-            //Testing the API
-            List<Employee> employees = await PeopleFetcher.GetFromApi();
+            List<Employee> employees;
+            Console.WriteLine("Would you like to input the data to generate the badges?");
+            string response = Console.ReadLine() ?? "";
+            if (response == "y")
+            {
+                // GetEmployees method call
+                employees = PeopleFetcher.GetEmployees();
 
-            // PrintEmployees method call
-            Util.PrintEmployees(employees);
+                // PrintEmployees method call
+                Util.PrintEmployees(employees);
 
-            // MakeCSV - method call
-            Util.MakeCSV(employees);
+                // MakeCSV - method call
+                Util.MakeCSV(employees);
 
-            // Make Badges - method call - asynchronous
-            await Util.MakeBadges(employees);
+                // Make Badges - method call - asynchronous
+                await Util.MakeBadges(employees);
+            }
+            if (response == "n")
+            {
+                //GetFromApi method call
+                employees = await PeopleFetcher.GetFromApi();
 
+                // PrintEmployees method call
+                Util.PrintEmployees(employees);
+
+                // MakeCSV - method call
+                Util.MakeCSV(employees);
+
+                // Make Badges - method call - asynchronous
+                await Util.MakeBadges(employees);
+            }
         }
-
     }
 }
